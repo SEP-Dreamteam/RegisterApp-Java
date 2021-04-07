b82acf6d8feb476d4270c05439a5d3bad36639ec
nathan fredricks <njfredr@uark.edu>
2021-04-07 12:18:59 -0500

    productLisint route controller change

--- src/main/java/edu/uark/registerapp/controllers/ProductRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductRestController.java
@@ -1,7 +1,12 @@
 package edu.uark.registerapp.controllers;
 
+import java.io.Console;
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.commands.products.ProductCreateCommand;
 import edu.uark.registerapp.commands.products.ProductDeleteCommand;
 import edu.uark.registerapp.commands.products.ProductUpdateCommand;
@@ -18,7 +25,7 @@ import edu.uark.registerapp.models.api.Product;
 
 @RestController
 @RequestMapping(value = "/api/product")
-public class ProductRestController {
+public class ProductRestController extends BaseRestController{
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public @ResponseBody ApiResponse createProduct(
 		@RequestBody final Product product
@@ -32,8 +39,25 @@ public class ProductRestController {
 	@RequestMapping(value = "/{productId}", method = RequestMethod.PUT)
 	public @ResponseBody ApiResponse updateProduct(
 		@PathVariable final UUID productId,
-		@RequestBody final Product product
+		@RequestBody final Product product,
+		final HttpServletRequest request,
+		final HttpServletResponse response
 	) {
+
+		ApiResponse canCreateEmployeeResponse;
+
+		try {
+			this.activeEmployeeExistsQuery.execute();
+
+			canCreateEmployeeResponse =
+				this.redirectUserNotElevated(request, response);
+		} catch (final NotFoundException e) {
+			canCreateEmployeeResponse = new ApiResponse();
+		}
+
+		if (!canCreateEmployeeResponse.getRedirectUrl().equals(StringUtils.EMPTY)) {
+			return canCreateEmployeeResponse;
+		}
 
 		return this.productUpdateCommand
 			.setProductId(productId)
@@ -62,4 +86,7 @@ public class ProductRestController {
 	
 	@Autowired
 	private ProductUpdateCommand productUpdateCommand;
+
+	@Autowired
+	private ActiveEmployeeExistsQuery activeEmployeeExistsQuery;
 }
